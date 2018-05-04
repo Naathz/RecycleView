f14f5912cc8dd8351e09eaecfc95a1de617301f1
logonrm <logonrm@fiap.com.br>
2018-05-04 09:48:30 -0300

    Initial commit

---
+++ app/src/main/java/nathalie/com/recycleview/Jogo.kt
@@ -0,0 +1,7 @@
+package nathalie.com.recycleview
+
+data class Jogo(val imagemId: Int, val titulo: String, val anoLancamento: Int, val descricao: String)
+//indicar quem são os construtores dessa classe, representado pelo val oQueÉ: seu tipo -> eles formam a classe
+
+//val é um valor que não pode ser alterado. Mais econômico
+//var é variável, pode ter seu valor alterado
